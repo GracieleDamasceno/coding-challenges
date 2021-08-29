8c613a29fc1081cb595642800756dac458d8d11a
Graciele Damasceno <56450271+GracieleDamasceno@users.noreply.github.com>
2021-08-29 18:02:40 -0300

    feat: problem 15 structure and solution

---
+++ week-02/challenge-15/README.md
@@ -0,0 +1,3 @@
+**Drawing Book**
+<br>
+The challenge statement - provided by HackerRank -  can be found [here](src/main/resources/drawing-book.pdf).
---
+++ week-02/challenge-15/src/main/java/problem/Solution15.java
@@ -0,0 +1,21 @@
+package problem;
+
+import java.io.*;
+
+public class Solution15 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int p = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result15.pageCount(n, p);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
